99ff3b0be16e9c1ef42a6cdf389f9a4924206bb1
Gustav-schnell <1302322212@ulife.com.br>
2024-07-04 16:36:27 -0300

    Add files via upload

---
+++ Cadastramento/src/main/java/Base/Site.java
@@ -0,0 +1,20 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeClass;
+
+public class Site
+{
+    public WebDriver driver;
+    public String url;
+    @BeforeClass
+    public void Before()
+    {
+        url = "";
+        driver = new EdgeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+    protected WebDriver GetDriver() {return driver;}
+}
---
+++ Cadastramento/src/test/java/Cadastramento.java
@@ -0,0 +1,46 @@
+import Base.Site;
+import Paginas.SiteCadastro;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Cadastramento extends Site
+{
+    SiteCadastro access = new SiteCadastro(GetDriver());
+    // Cenário 1 - Caso 5
+    @Test
+    public void NomeCompletocomCemLetras()
+    {
+        access.CampoNomeCompleto("Wilhelm Friedrich Ferdinand Maximilian Heinrich Joseph Maria von Hohenzollern-Sigmaringen-Wittelsbach")
+                .CampoSenha("123456")
+                .CampoEmail("wilhelm@gmail.com")
+                .CampoEndereco("Rua das Flores, 1234, Bairro dos Açorianos")
+                .CampoEstado("Rio Grande do Sul")
+                .CampoMunicipio("Porto Alegre")
+                .CampoPerfil1().CampoPerfil3();
+
+        String Resultado = access.BuscarResultado();
+    }
+
+    // Cenário 1 - Caso 11
+    @Test
+    public void MunicipioAntesdeEstado()
+    {
+        access.CampoNomeCompleto("Whilhelm Heinrich")
+                .CampoSenha("123456")
+                .CampoEmail("wilhelm@gmail.com")
+                .CampoEndereco("Rua das Flores, 1234, Bairo dos Açorianos")
+                .CampoPerfil1().CampoPerfil2()
+                .CampoMunicipio("Porto Alegre");
+
+        String Resultado = access.BuscarResultado();
+
+        /*
+        Assert.assertTrue(Resultado.contains("Wilhelm Heinrich"));
+        Assert.assertTrue(Resultado.contains("1234"));
+
+
+         */
+
+    }
+
+}
